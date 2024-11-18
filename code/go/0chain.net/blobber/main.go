a253dcb29a31d6c96391d23754c8524efd02f69d
Hitenjain14 <hitenjain146@gmail.com>
2024-11-19 02:20:45 +0530

    remove register check for enterprise

--- code/go/0chain.net/blobber/main.go
+++ code/go/0chain.net/blobber/main.go
@@ -43,14 +43,11 @@ func main() {
 	// prepare is to configure more.
 	// when enabled "// +build integration_tests", this sets blobber for conductor tests.
 	prepareBlobber(node.Self.ID)
-
+	common.SetBlobberRegistered(true)
 	go func() {
 		if err := registerOnChain(); err != nil {
 			logging.Logger.Error("Error register on blockchain" + err.Error())
-			panic(err)
 		}
-
-		common.SetBlobberRegistered(true)
 	}()
 
 	if err := setStorageScConfigFromChain(); err != nil {
